81d6f715ae285a91d5ac3cb39c72a58f2742bd7a
mirr <121735620+mirr-x@users.noreply.github.com>
2024-07-18 11:53:29 +0100

    Add files via upload

---
+++ z_1337/c08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molahrac <molahrac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 05:26:21 by molahrac          #+#    #+#             */
+/*   Updated: 2024/07/15 05:27:15 by molahrac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
